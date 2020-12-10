018a446b4754a16ea67fb1a739c6d78c0c2e3105
johnOdia <efosajohnodia@gmail.com>
2020-12-09 21:55:20 -1100

    added function wrapper to avoid sorting the array everytime in binarysearchrecusive

--- search.js
+++ search.js
@@ -1,41 +1,45 @@
-const linearSearch = (array,element) => {
-    for (let i = 0; i < array.length; i++){
-        if(array[i] === element) return i
+const linearSearch = (array, element) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) return i
     }
     return -1
 }
 
-const linearSearchRecursive = (array,element,index  = 0)=> {
+const linearSearchRecursive = (array, element, index = 0) => {
     if (index > array.length) return -1
-    else if(element === array[index]) return index
-    return linearSearch(array,element,index += 1)
+    else if (element === array[index]) return index
+    return linearSearch(array, element, index += 1)
 }
 
-const binarySearch = (array,element) => {
+const binarySearch = (array, element) => {
     let low = 0
-    let high = array.length-1
-    let sortedArray = array.sort((a,b) => a-b)
+    let high = array.length - 1
+    let sortedArray = array.sort((a, b) => a - b)
 
-    while(low <= high){
-        let mid = Math.floor((high+low)/2)
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2)
 
-        if(sortedArray[mid] === element) return mid
+        if (sortedArray[mid] === element) return mid
 
-        if(element > array[mid]) low = mid + 1
-        
-        else high = mid -1
+        if (element > array[mid]) low = mid + 1
+
+        else high = mid - 1
 
     }
 
     return -1
 }
 
-const binarySearchRecursive = (array,val,high = array.length,low = 0) =>{
-    sortedArray = array.sort((a,b) => a-b)
-    
+const binarySearchRecursive = (array, val) => {
+    sortedArray = array.sort((a, b) => a - b);
+    return binarySearchRecursiveUtil(sortedArray, val, sortedArray.length, 0);
+}
+
+const binarySearchRecursiveUtil = (array,val,high = array.length,low = 0) =>{
+
     let mid = Math.floor((high+low)/2)
-  
-    if(sortedArray[mid] === val) return mid
+    
+    if(array[mid] === val) return mid
     
     if(low >= high) return -1
     
@@ -45,9 +49,8 @@ const binarySearchRecursive = (array,val,high = array.length,low = 0) =>{
         high = mid -1
     }
     
-    return binarySearchRecursive(array,val,low, high)
-    
-  }
+    return binarySearchRecursiveUtil(array,val,low, high)
+    }
 
 
-  binarySearchRecursive([1, 2, 3, 4], 4)
+binarySearchRecursive([1, 2, 3, 4], 4)
